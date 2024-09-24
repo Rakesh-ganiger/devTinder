1657a225e0fd4c82ce06124786456a7ddaf9c216
Rakesh-ganiger <ganigerrakesh1709@gmail.com>
2024-09-24 17:00:48 +0530

    exploring GET POST DELETE

--- src/app.js
+++ src/app.js
@@ -1,16 +1,37 @@
 const express=require("express");
 const app=express();
 
-app.use("/test",(req,res)=>{
-    res.send("hello hello hello ")
+
+// app.use("/user", (req,res)=>{
+//     res.send("hahahahahaah")
+// })
+
+app.patch("/user",(req,res)=>{
+    res.send("fatch data sucessfully")
 })
+
+app.get("/user",(req,res)=>{
+    res.send({firstname:"Rakesh",lastname:"ganiger"})
+})
+app.post("/user",(req,res)=>{
+    res.send("data sucessfully saved to database")
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("data deleted sucessfully")
+})
+
+
 app.use("/hello",(req,res)=>{
     res.send("hello from the rakesh how are u")
 })
 
-app.use((req,res)=>{
-    res.send("hello from the server")
-})
+// app.use("/",(req,res)=>{
+//     res.send("Namaste node")
+// })
+
+
+
 
 app.listen(7777,()=>{
     console.log("server created suceesfully")
