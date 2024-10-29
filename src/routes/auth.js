a152aff25676b0cbfeca31e4dbd54fca96ff9c72
Rakesh-ganiger <ganigerrakesh1709@gmail.com>
2024-10-29 16:35:29 +0530

    logout+profile/edit api

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -76,4 +76,11 @@ authRouter.post("/login",async(req,res)=>{
     }
 })
 
+authRouter.post("/logout",async(req,res)=>{
+    res.cookie("token",null,{
+        expires:new Date(Date.now())
+    });
+    res.send("Logout sucessful")
+})
+
 module.exports=authRouter;
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -13,6 +13,23 @@ const validateSignupData=(req)=>{
         throw new Error("Enter a strong password")
     }
 }
+const validateEditProfileData=(req)=>{
+    const allowedEditFields=[
+        "firstName",
+        "lastName",
+        "gender",
+        "email",
+        "about",
+        "photourl",
+        "age",
+        "skills"]
+    const isEditAllowed=Object.keys(req.body).every((field)=>
+        allowedEditFields.includes(field)
+);
+return isEditAllowed;
+
+}
 module.exports={
     validateSignupData,
+    validateEditProfileData
 }
