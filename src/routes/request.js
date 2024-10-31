6fb4607be53d668cfa59be0335988dcd49ef4900
Rakesh-ganiger <ganigerrakesh1709@gmail.com>
2024-10-31 15:24:58 +0530

    design api for review(accept or reject) connection request

--- src/routes/request.js
+++ src/routes/request.js
@@ -50,5 +50,36 @@ catch(error){
 }
 })
 
+requestRouter.post("/request/review/:status/:requestId",userAuth,async(req,res)=>{
+    try {
+        const loggedInUser=req.user;
+        const {status,requestId}=req.params;
+
+        const allowedStatus=["accepted","rejected"]
+        if(!allowedStatus.includes(status)){
+            return res.status(400).json({message:"Invalide status"})
+        }
+        const connectionRequest= await ConnectionRequest.findOne({
+            _id:requestId,
+            toUserId:loggedInUser,
+            status:"interested"
+        })
+
+        if(!connectionRequest){
+            return res.status(400).json({message:"connection request not found"})
+        }
+        
+        connectionRequest.status=status;
+        const data=await connectionRequest.save();
+        res.json({message:"connection request " + status ,data})
+        
+
+        
+    } catch (error) {
+        res.status(400).send("ERROR! " + error.message)
+        
+    }
+})
+
 module.exports=requestRouter;
 
