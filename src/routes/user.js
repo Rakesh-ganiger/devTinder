b5161f5f5124220dbf7f305be365659dd96f1d14
Rakesh-ganiger <ganigerrakesh1709@gmail.com>
2024-11-01 20:26:41 +0530

    feed api and also used page(limit and page)

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express= require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest= require("../models/connectionRequest");
+const User = require("../models/user");
 const userRouter= express.Router();
 const USER_SAFE_DATA= "firstName lastName age gender photourl about";
 userRouter.get("/user/request/recieved", userAuth, async(req,res) => {
@@ -40,6 +41,42 @@ userRouter.get("/user/connections", userAuth, async(req,res)=>{
     } catch (error) {
         res.status(400).json("ERROR!" + error.message)
         
+    }
+})
+
+userRouter.get("/feed",userAuth,async(req,res)=>{
+    try {
+        const loggedInUser= req.user;
+
+        const page=parseInt(req.query.page) || 1;
+        let limit=parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit
+        const skip=(page-1) * limit;
+
+
+
+        const connectionRequests= await ConnectionRequest.find({
+            $or:[{fromUserId: loggedInUser._id},
+                {toUserId: loggedInUser._id}
+            ]
+        }).select("fromUserId toUserId")
+
+    const hiddenUserFromFeed = new Set();
+    connectionRequests.forEach((req)=>{
+        hiddenUserFromFeed.add(req.fromUserId.toString()),
+        hiddenUserFromFeed.add(req.toUserId.toString())
+    });
+    const users = await User.find({
+        $and: [{
+            _id: { $nin: Array.from(hiddenUserFromFeed)}},
+           {_id: { $ne:loggedInUser._id}}]
+    }).select(USER_SAFE_DATA)
+    .skip(skip)
+    .limit(limit)
+
+    res.json({data : users})
+    } catch (error) {
+        res.status(400).json("ERROR!" +error.message)
     }
 })
 
